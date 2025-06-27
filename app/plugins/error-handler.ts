f58f8ac3af68059311ce80a57571a1a0e04beacc
RedStars071 <redtwoghost@gmail.com>
2025-06-27 14:45:04 +0200

    refactor: simplify error handler configuration in error-handler.ts for improved readability

--- app/plugins/error-handler.ts
+++ app/plugins/error-handler.ts
@@ -2,17 +2,13 @@ export default defineNuxtPlugin(async (nuxtApp) => {
 	const { handleError, handleVueError } = await useErrorHandler();
 
 	// Global Vue error handler
-	nuxtApp.vueApp.config.errorHandler = (error, instance, info) => {
-		handleVueError(error, instance, info);
-	};
+	nuxtApp.vueApp.config.errorHandler = (error, instance, info) => handleVueError(error, instance, info);
 
 	// Nuxt Vue error hook
-	nuxtApp.hook('vue:error', (error, instance, info) => {
-		handleVueError(error, instance, info);
-	});
+	nuxtApp.hook('vue:error', (error, instance, info) => handleVueError(error, instance, info));
 
 	// Nuxt app error hook
-	nuxtApp.hook('app:error', (error) => {
+	nuxtApp.hook('app:error', (error) =>
 		handleError(error, {
 			logToConsole: true,
 			showToast: true,
@@ -20,6 +16,5 @@ export default defineNuxtPlugin(async (nuxtApp) => {
 				errorType: 'app',
 				source: 'nuxt-app'
 			}
-		});
-	});
+		})	);
 });

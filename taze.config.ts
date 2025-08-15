85467d576a0dc8cadbdf40ac564140c1a39d4ef5
RedStars071 <redtwoghost@gmail.com>
2025-08-15 19:32:22 +0200

    chore: update taze config to ignore typescript and pin vue to latest

--- taze.config.ts
+++ taze.config.ts
@@ -4,4 +4,12 @@ export default defineConfig({
   write: true,
   // run `npm install` or `yarn install` right after bumping
   install: true,
+  packageMode: {
+    "typescript": "ignore",
+    // regex starts and ends with '/'
+    "/vue/": "latest",
+  },
+  depFields: {
+    overrides: false,
+  },
 });

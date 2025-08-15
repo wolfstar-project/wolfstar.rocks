0aa3ddaacb5f8d9d9a24d4129a3bbd520b11e399
RedStars071 <redtwoghost@gmail.com>
2025-08-15 19:59:40 +0200

    feat(app): add update notification hook

--- app/app.vue
+++ app/app.vue
@@ -11,6 +11,22 @@
 const router = useRouter();
 const appName = ref<"wolfstar" | "staryl">("wolfstar");
 
+const nuxtApp = useNuxtApp();
+nuxtApp.hooks.hookOnce("app:manifest:update", () => {
+  useToast().add({
+    title: "Needs to update",
+    color: "info",
+    description: "Update the app to the latest version",
+    closeIcon: "ic:round-close",
+    actions: [{
+      color: "neutral",
+      activeColor: "primary",
+      label: "Update",
+      onClick: (_event) => window.location.reload(),
+      icon: "ic:round-warning",
+    }],
+  });
+});
 // Watch for route changes to update appName
 watch(
   router.currentRoute,

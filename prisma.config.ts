75de9792e014c06d5f7f1d25951c9b46e27ea7d7
RedStars071 <redtwoghost@gmail.com>
2025-09-27 18:36:10 +0200

    feat(database): add prisma configuration and schema files

---
+++ prisma.config.ts
@@ -0,0 +1,11 @@
+/**
+ * Prisma configuration file
+ * See: https://www.prisma.io/docs/orm/reference/prisma-config-reference
+ */
+
+import path from "node:path";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: path.join("server", "database", "schema.prisma"),
+});

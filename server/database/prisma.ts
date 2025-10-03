27de31133540d464634cf56b5ba393546e80bdeb
RedStars071 <redtwoghost@gmail.com>
2025-10-03 16:00:54 +0200

    fix(prisma.ts): remove DATABASE_URL check and use non-null assertion

--- server/database/prisma.ts
+++ server/database/prisma.ts
@@ -13,8 +13,5 @@ export function getDb({ connectionString }: GetDbParams) {
   return prisma;
 }
 const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL environment variable is required");
-}
-const prisma = getDb({ connectionString: databaseUrl });
+const prisma = getDb({ connectionString: databaseUrl! });
 export default prisma;

41e587cee23b205a9d28ac7d03f4f7facfcffaa3
RedStars071 <redtwoghost@gmail.com>
2025-10-26 14:24:07 +0100

    chore: adjust mention tag spacing, weight and colors

--- app/components/discord/mention.vue
+++ app/components/discord/mention.vue
@@ -15,13 +15,13 @@ withDefaults(defineProps<{ kind?: "mention" | "app" }>(), {
 <style scoped>
 @reference "@/assets/css/main.css";
 .tag {
-	@apply gap-1 rounded-md px-0.5 font-normal;
-	background-color: hsla(235, 85.6%, 64.7%, 0.3);
-	color: hsl(235, 86.2%, 88.6%, 1);
+	@apply gap-1 rounded-md px-1 py-0.5 font-medium;
+	background-color: hsla(235, 85.6%, 64.7%, 0.5);
+	color: hsl(235, 86.2%, 95%, 1);
 
 	@media (prefers-color-scheme: light) {
-		background-color: hsla(235, 85.6%, 64.7%, 0.15);
-		color: hsl(235, 86.2%, 64.7%, 1);
+		background-color: hsla(235, 85.6%, 64.7%, 0.25);
+		color: hsl(235, 86.2%, 50%, 1);
 	}
 }
 
@@ -33,9 +33,10 @@ withDefaults(defineProps<{ kind?: "mention" | "app" }>(), {
 .tag:focus-visible {
 	background-color: hsl(235, 85.6%, 64.7%, 1);
 	color: hsl(0, 0%, 100%, 1);
+	@apply transition-colors duration-150;
 
 	@media (prefers-color-scheme: light) {
-		background-color: hsl(235, 85.6%, 64.7%, 0.7);
+		background-color: hsl(235, 85.6%, 55%, 1);
 		color: hsl(0, 0%, 100%, 1);
 	}
 }

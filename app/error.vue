3940e4b9b6791e36d76c3ad79c85c81cb5881f24
RedStar <redstar071@proton.me>
2025-12-29 15:47:18 +0100

    refactor: simplify error component by removing unnecessary props and unused styles

--- app/error.vue
+++ app/error.vue
@@ -8,7 +8,7 @@
     <NuxtLayout>
       <UMain>
         <UError
-          :error="error"
+          :error
         />
       </UMain>
     </NuxtLayout>
@@ -23,8 +23,6 @@ const { error } = defineProps<{
   error: NuxtError;
 }>();
 
-logger.error(`[${error.statusCode}] ${error.statusMessage}`);
-
 // SEO and meta configuration
 useSeoMetadata({
   title: error.statusCode.toString(),
@@ -32,51 +30,3 @@ useSeoMetadata({
   shouldSeoImage: true,
 });
 </script>
-
-<style scoped>
-@reference '@/assets/css/main.css';
-
-@keyframes blink {
-	0%,
-	100% {
-		opacity: 1;
-	}
-	50% {
-		opacity: 0;
-	}
-}
-
-@keyframes hang {
-	0%,
-	100% {
-		transform: rotate(15deg) translateY(0);
-	}
-	50% {
-		transform: rotate(-15deg) translateY(-10px);
-	}
-}
-
-@keyframes pulsebox {
-	0%,
-	100% {
-		box-shadow: 0 0 0 0 #ff5d5b44;
-	}
-	50% {
-		box-shadow: 0 0 16px 4px #ff5d5b88;
-	}
-}
-
-.animate-blink {
-	animation: blink 1s infinite;
-}
-.animate-hang {
-	animation: hang 4s infinite;
-}
-.animate-pulsebox {
-	animation: pulsebox 3s infinite ease-in-out;
-}
-
-.perspective-1000 {
-	perspective: 1000px;
-}
-</style>

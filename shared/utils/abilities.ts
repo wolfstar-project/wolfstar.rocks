09226642ebd90b3f93a89c4cb369279faa18332a
RedStar <redstar071@proton.me>
2026-01-21 16:54:23 +0100

    refactor: update member type in isAdmin and manageAbility functions for improved type safety

--- shared/utils/abilities.ts
+++ shared/utils/abilities.ts
@@ -1,19 +1,18 @@
-import type { FlattenedMember } from "#shared/types/discord";
-import type { APIGuild } from "discord-api-types/v10";
+import type { APIGuild, APIGuildMember } from "discord-api-types/v10";
 import { PermissionsBits } from "#shared/utils/bits";
 import { hasAtLeastOneKeyInMap } from "@sapphire/utilities";
 import { PermissionFlagsBits } from "discord-api-types/v10";
 import { defineAbility } from "nuxt-authorization/utils";
 import { readSettings, readSettingsPermissionNodes } from "~~/server/database";
 
-function isAdmin(member: FlattenedMember, roles: readonly string[]): boolean {
+function isAdmin(member: APIGuildMember, roles: readonly string[]): boolean {
   const memberRolePermissions = BigInt((member as unknown as { permissions: string }).permissions);
   return roles.length === 0
     ? PermissionsBits.has(memberRolePermissions, PermissionFlagsBits.ManageGuild)
-    : hasAtLeastOneKeyInMap(new Map(roles.map(role => [role, true])), member.roles.map(r => r.id));
+    : hasAtLeastOneKeyInMap(new Map(roles.map(role => [role, true])), member.roles.map(r => r));
 }
 
-export const manageAbility = defineAbility({ allowGuest: false }, async (guild: APIGuild, member: FlattenedMember) => {
+export const manageAbility = defineAbility({ allowGuest: false }, async (guild: APIGuild, member: APIGuildMember) => {
   if (!member.user || !member.user.id) {
     return false;
   }

21775b3f4f7e538164fbf9b6288f50a4f4b72a22
RedStar <redstar071@proton.me>
2026-02-04 15:03:17 +0100

    feat: enhance CI workflow and add Playwright testing scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,23 +3,32 @@ import process from "node:process";
 import { fileURLToPath } from "node:url";
 import { defineConfig, devices } from "@playwright/test";
 
+const baseURL = "http://localhost:5678";
+
 export default defineConfig<ConfigOptions>({
-  testDir: "./test",
+  testDir: "./test/e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   timeout: 120_000,
+  webServer: {
+    command: "pnpm start:playwright:webserver",
+    url: baseURL,
+    reuseExistingServer: false,
+    timeout: 60_000,
+  },
   use: {
+    baseURL,
     trace: "on-first-retry",
     nuxt: {
       rootDir: fileURLToPath(new URL(".", import.meta.url)),
+      host: baseURL,
     },
   },
   projects: [
     {
-      name: "chromium",
+      name: "chromium-headless-shell",
       use: { ...devices["Desktop Chrome"] },
     },
   ],

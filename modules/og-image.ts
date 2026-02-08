96e4ad6502ecc14d151df8e100cf8aad4b95c40e
RedStar <redstar071@proton.me>
2026-02-08 17:03:57 +0100

    perf: do not include nuxt-og-image templates

---
+++ modules/og-image.ts
@@ -0,0 +1,16 @@
+import { defineNuxtModule } from "nuxt/kit";
+
+export default defineNuxtModule({
+  meta: {
+    name: "og-image-tweaks",
+  },
+  setup(_, nuxt) {
+    nuxt.hook("components:extend", components => {
+      for (const component of [...components].toReversed()) {
+        if (component.filePath.includes("og-image")) {
+          components.splice(components.indexOf(component), 1);
+        }
+      }
+    });
+  },
+});

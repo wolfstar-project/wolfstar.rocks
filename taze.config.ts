b7168a9bfae59ee9f8a99c078fc7121c18666f32
RedStar <redstar071@proton.me>
2026-02-09 16:24:18 +0100

    refactor: remove typescript from packageMode in taze.config.ts

--- taze.config.ts
+++ taze.config.ts
@@ -1,25 +1,24 @@
 import { defineConfig } from "taze";
 
 export default defineConfig({
-  write: true,
-  // run `npm install` or `yarn install` right after bumping
-  install: true,
-  update: true,
-  recursive: true,
-  includeLocked: true,
-  interactive: true,
-  packageMode: {
-    "typescript": "ignore",
-    "@prisma/client": "ignore",
-    "@prisma/adapter-pg": "ignore",
-    "prisma-json-types-generator": "ignore",
-    "prisma": "ignore",
-    // regex starts and ends with '/'
-    "/vue/": "latest",
-    "/@vueuse/": "latest",
-    "vue-tsc": "minor",
-  },
-  depFields: {
-    overrides: false,
-  },
+	write: true,
+	// run `npm install` or `yarn install` right after bumping
+	install: true,
+	update: true,
+	recursive: true,
+	includeLocked: true,
+	interactive: true,
+	packageMode: {
+		"@prisma/client": "ignore",
+		"@prisma/adapter-pg": "ignore",
+		"prisma-json-types-generator": "ignore",
+		prisma: "ignore",
+		// regex starts and ends with '/'
+		"/vue/": "latest",
+		"/@vueuse/": "latest",
+		"vue-tsc": "minor",
+	},
+	depFields: {
+		overrides: false,
+	},
 });

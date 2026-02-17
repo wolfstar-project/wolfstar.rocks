1e87e3f8b9045c29fb7b93554131ef7e595b393b
RedStar <redstar071@proton.me>
2026-02-17 18:32:28 +0100

    refactor(evlog-drain): simplify Sentry drain initialization logic

--- server/plugins/evlog-drain.ts
+++ server/plugins/evlog-drain.ts
@@ -4,13 +4,10 @@ import { createSentryDrain } from "evlog/sentry";
 
 export default defineNitroPlugin((nitroApp) => {
 	const pipeline = createDrainPipeline<DrainContext>();
-	const drain = pipeline(
-		createSentryDrain({
-			dsn: runtimeConfig.public.sentry.dsn,
-			environment: runtimeConfig.public.environment,
-		}),
-	);
+	if (runtimeConfig.public.sentry.dsn) {
+		const drain = pipeline(createSentryDrain());
 
-	nitroApp.hooks.hook("evlog:drain", drain);
-	nitroApp.hooks.hook("close", () => drain.flush());
+		nitroApp.hooks.hook("evlog:drain", drain);
+		nitroApp.hooks.hook("close", () => drain.flush());
+	}
 });

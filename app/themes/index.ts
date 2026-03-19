0d2eb5339e314b66db8e0fe8a7db39a3f7391860
RedStar <redstar071@proton.me>
2026-03-19 17:58:49 +0100

    refactor: remove unused input and select components and dropdown menu

--- app/themes/index.ts
+++ app/themes/index.ts
@@ -7,13 +7,9 @@ import fieldGroup from "./field-group";
 import footer from "./footer";
 import footerColumns from "./footer-columns";
 import header from "./header";
-import input from "./input";
-import inputMenu from "./input-menu";
 import link from "./link";
 import main from "./main";
 import navigationMenu from "./navigation-menu";
-import select from "./select";
-import selectMenu from "./select-menu";
 import skeleton from "./skeleton";
 import tabs from "./tabs";
 import toaster from "./toaster";
@@ -28,13 +24,9 @@ const ui = {
 	footer,
 	footerColumns,
 	header,
-	input,
-	inputMenu,
 	link,
 	main,
 	navigationMenu,
-	select,
-	selectMenu,
 	skeleton,
 	tabs,
 	toaster,

a57d2d87d4973e2f4273f879ca832ac45afc4d55
RedStar <redstar071@proton.me>
2026-07-28 17:53:15 +0200

    chore: configure skew-protection storage for Netlify and local dev (#326)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -339,6 +339,10 @@ export default defineNuxtConfig({
 				base: "./.cache/fetch",
 				driver: "fsLite",
 			},
+			"skew-protection": {
+				base: "./.cache/skew-protection",
+				driver: "fsLite",
+			},
 			"wolfstar:ratelimiter": {
 				base: "./.cache/ratelimiter",
 				driver: "fsLite",
